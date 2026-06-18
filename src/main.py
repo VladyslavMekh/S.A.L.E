e0bd01a6f2335f01e02c29f272685061893d8fce
Vladyslav Mekh <vladmekh101@gmail.com>
2026-06-18 19:53:08 +0200

    Rename output JSON files for clarity

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from sale_app import SALEApp
-
-if __name__ == "__main__":
-    app = SALEApp("src/data/logs/logs.usrlog",
-                  "src/stages/second/pipeline_config.json")
-    app.run()
