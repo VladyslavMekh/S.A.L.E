b821ada66a4725535c3b5de7199163ffc75f7a97
Vladyslav Mekh <vladmekh101@gmail.com>
2026-06-18 19:52:32 +0200

    Update log file path in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from sale_app import SALEApp
 
 if __name__ == "__main__":
-    app = SALEApp("src/data/logs/s33307.usrlog",
+    app = SALEApp("src/data/logs/logs.usrlog",
                   "src/stages/second/pipeline_config.json")
     app.run()
